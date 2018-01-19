2bab04419590afa4731309651901278aad545cb5
PetitBesson <35603264+PetitBesson@users.noreply.github.com>
2018-01-19 15:33:21 +0100

    Add files via upload

---
+++ Encodage_message.py
@@ -0,0 +1,27 @@
+#Codage du message 1 caractère = 8 bits
+
+def encodage(m): # message en string donne une liste d'entiers 0 ou 1
+    if type(m) != str :
+        raise TypeError
+    M = list(m) # on split tout en caractères individuels
+    Mord = [ord(M[i]) for i in range(len(M))]
+    Mbin = [bin(Mord[i]) for i in range(len(M))]
+    # traitement du Mbin pour enlever le '0b' du début et taille constante
+    Mfin=[]
+    for binaire in Mbin:
+        binaire=binaire[2:]
+        n=len(binaire)
+        if n>8:
+            return 'Erreur len(binaire)'
+        for i in range(8-n):
+            Mfin.append(0)
+        for i in binaire:
+            Mfin.append(int(i))
+    print("m=",m)
+    print("M=",M)
+    print("Mord=",Mord)
+    print("Mbin=",Mbin)
+    return Mfin
+    
+def decodage
+    
