439069b97063fb9ed3a79edc36217d35ec740790
PetitBesson <petitbesson@gmail.com>
2018-02-09 17:04:08 +0100

    On a notre serveur

--- Code - Serveurs/Test Serveurs.py
+++ Code - Serveurs/Test Serveurs.py
@@ -1,6 +1,74 @@
-#Lien : https://openclassrooms.com/courses/apprenez-a-programmer-en-python/le-reseau
+#Lien : http://www.supinfo.com/articles/single/1232-reseau-python
 
-## Test Client
+## Threads
+
+from threading import Thread
+
+class ThreadClientReception(Thread):
+    
+    def __init__(self,connexion):
+        Thread.__init__(self)
+        self.connexion=connexion
+        
+    def run(self):
+        Continue=True
+        while Continue :
+            message_recu=self.connexion.recv(1024).decode()
+            print(message_recu)
+            if message_recu='Deconnexion':
+                Continue=False
+            
+class ThreadClientEmission(Thread):
+    
+    def __init__(self,connexion):
+        Thread.__init__(self)
+        self.connexion=connexion
+    
+    def run(self):
+        Continue=True
+        while Continue:
+            message=input()
+            if message != 'Deconnexion':
+                self.connexion.send(message.encode())
+            else:
+                self.connexion.close()
+                Continue=False
+
+class ThreadServeurReception(Thread):
+    
+    def __init__(self,connexion_principale,connexion_client):
+        Thread.__init__(self)
+        self.connexion_principale=connexion_principale
+        self.connexion_cient=connexion_client
+        
+    def run(self):
+        Continue=True
+        while Continue :
+            message_recu=self.connexion_client.recv(1024).decode()
+            print(message_recu)
+            if message_recu='Deconnexion':
+                Continue=False
+                connexion_client.send(b"Deconnexion")
+                connexion_client.close()
+                connexion_principale.close()
+                
+class ThreadServeurEmission(Thread):
+    
+    def __init__(self,connexion):
+        Thread.__init__(self)
+        self.connexion=connexion
+    
+    def run(self):
+        Continue=True
+        while Continue:
+            message=input()
+            if message != 'Deconnexion':
+                self.connexion.send(message.encode())
+            else:
+                self.connexion.close()
+                Continue=False
+
+## Partie Client
 
 import socket
 
@@ -10,21 +78,16 @@ port = 12800
 connexion_avec_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connexion_avec_serveur.connect((hote, port))
 print("Connexion établie avec le serveur sur le port {}".format(port))
+thread_reception=ThreadClientReception(connexion_avec_serveur)
+thread_emission=ThreadClientEmission(connexion_avec_serveur)
 
-msg_a_envoyer = b""
-while msg_a_envoyer != b"fin":
-    msg_a_envoyer = input("> ")
-    # Peut planter si vous tapez des caractères spéciaux
-    msg_a_envoyer = msg_a_envoyer.encode()
-    # On envoie le message
-    connexion_avec_serveur.send(msg_a_envoyer)
-    msg_recu = connexion_avec_serveur.recv(1024)
-    print(msg_recu.decode()) # Là encore, peut planter s'il y a des accents
+thread_reception.start()
+thread_emission.start()
 
-print("Fermeture de la connexion")
-connexion_avec_serveur.close()
+thread_reception.join()
+thread_emission.join()
 
-## Test serveur
+## Partie Serveur
 
 import socket
 
@@ -37,6 +100,7 @@ connexion_principale.listen(5)
 print("Le serveur écoute à présent sur le port {}".format(port))
 
 connexion_avec_client, infos_connexion = connexion_principale.accept()
+print("Connecté avec le client {}".format(infos_connexion))
 
 msg_recu = b""
 while msg_recu != b"fin":
@@ -49,61 +113,3 @@ while msg_recu != b"fin":
 print("Fermeture de la connexion")
 connexion_avec_client.close()
 connexion_principale.close()
-
-## Test serveur elabore
-
-import socket
-import select
-
-hote = ''
-port = 12800
-
-connexion_principale = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connexion_principale.bind((hote, port))
-connexion_principale.listen(5)
-print("Le serveur écoute à présent sur le port {}".format(port))
-
-serveur_lance = True
-clients_connectes = []
-while serveur_lance:
-    # On va vérifier que de nouveaux clients ne demandent pas à se connecter
-    # Pour cela, on écoute la connexion_principale en lecture
-    # On attend maximum 50ms
-    connexions_demandees, wlist, xlist = select.select([connexion_principale],
-        [], [], 0.05)
-    
-    for connexion in connexions_demandees:
-        connexion_avec_client, infos_connexion = connexion.accept()
-        # On ajoute le socket connecté à la liste des clients
-        clients_connectes.append(connexion_avec_client)
-    
-    # Maintenant, on écoute la liste des clients connectés
-    # Les clients renvoyés par select sont ceux devant être lus (recv)
-    # On attend là encore 50ms maximum
-    # On enferme l'appel à select.select dans un bloc try
-    # En effet, si la liste de clients connectés est vide, une exception
-    # Peut être levée
-    clients_a_lire = []
-    try:
-        clients_a_lire, wlist, xlist = select.select(clients_connectes,
-                [], [], 0.05)
-    except select.error:
-        pass
-    else:
-        # On parcourt la liste des clients à lire
-        for client in clients_a_lire:
-            # Client est de type socket
-            msg_recu = client.recv(1024)
-            # Peut planter si le message contient des caractères spéciaux
-            msg_recu = msg_recu.decode()
-            print("Reçu {}".format(msg_recu))
-            client.send(b"5 / 5")
-            if msg_recu == "fin":
-                serveur_lance = False
-
-print("Fermeture des connexions")
-for client in clients_connectes:
-    client.close()
-
-connexion_principale.close()
-
