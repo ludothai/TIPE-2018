05fd696225402576bfd4160ac32a9180e7eee1ed
PetitBesson <35603264+PetitBesson@users.noreply.github.com>
2018-02-10 15:30:02 +0100

    Parties client et serveur scindées

---
+++ Code - Serveurs/Client.py
@@ -0,0 +1,53 @@
+#Lien : http://www.supinfo.com/articles/single/1232-reseau-python
+
+# Threads
+
+from threading import Thread
+
+class ThreadClientReception(Thread):
+    
+    def __init__(self,connexion_serveur):
+        Thread.__init__(self)
+        self.connexion_serveur=connexion_serveur
+        
+    def run(self):
+        Continue=True
+        while Continue:
+            message_recu=self.connexion_serveur.recv(1024).decode()
+            print(message_recu)
+            if message_recu=='Deconnexion':
+                Continue=False
+        self.connexion_serveur.send(b'Deconnexion')
+        self.connexion_serveur.close()
+            
+class ThreadClientEmission(Thread):
+    
+    def __init__(self,connexion_serveur):
+        Thread.__init__(self)
+        self.connexion_serveur=connexion_serveur
+    
+    def run(self):
+        
+        while True:
+            message=input()
+            self.connexion_serveur.send(message.encode())
+
+# Client
+
+import socket
+
+hote = "192.168.1.16"
+port = 12800
+
+connexion_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connexion_serveur.connect((hote, port))
+print("Connexion établie avec le serveur sur le port {}".format(port))
+
+thread_reception=ThreadClientReception(connexion_serveur)
+thread_emission=ThreadClientEmission(connexion_serveur)
+
+Continue=True
+thread_reception.start()
+thread_emission.start()
+
+thread_reception.join()
--- Code - Serveurs/Serveur.py
+++ Code - Serveurs/Serveur.py
@@ -1,37 +1,8 @@
 #Lien : http://www.supinfo.com/articles/single/1232-reseau-python
 
-## Threads
+# Threads
 
 from threading import Thread
-
-class ThreadClientReception(Thread):
-    
-    def __init__(self,connexion_serveur):
-        Thread.__init__(self)
-        self.connexion_serveur=connexion_serveur
-        
-    def run(self):
-        Continue=True
-        while Continue:
-            message_recu=self.connexion_serveur.recv(1024).decode()
-            print(message_recu)
-            if message_recu=='Deconnexion':
-                Continue=False
-        self.connexion_serveur.send(b'Deconnexion')
-        self.connexion_serveur.close()
-            
-class ThreadClientEmission(Thread):
-    
-    def __init__(self,connexion_serveur):
-        Thread.__init__(self)
-        self.connexion_serveur=connexion_serveur
-    
-    def run(self):
-        
-        while True:
-            message=input()
-            self.connexion_serveur.send(message.encode())
-
 
 class ThreadServeurReception(Thread):
     
@@ -62,32 +33,12 @@ class ThreadServeurEmission(Thread):
             message=input()
             self.connexion_client.send(message.encode())
 
-## Partie Client
+
+# Serveur
 
 import socket
 
-hote = "localhost"
-port = 12800
-
-connexion_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connexion_serveur.connect((hote, port))
-print("Connexion établie avec le serveur sur le port {}".format(port))
-
-thread_reception=ThreadClientReception(connexion_serveur)
-thread_emission=ThreadClientEmission(connexion_serveur)
-
-Continue=True
-thread_reception.start()
-thread_emission.start()
-
-thread_reception.join()
-
-
-## Partie Serveur
-
-import socket
-
-hote = ''
+hote = '192.168.1.16'
 port = 12800
 
 connexion_principale = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
