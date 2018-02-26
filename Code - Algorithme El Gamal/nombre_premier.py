9249fc47faad0edcb4bea794e7a52e747c330dd1
PetitBesson <35603264+PetitBesson@users.noreply.github.com>
2018-02-26 12:04:47 +0100

    travail sur les nombres premiers

--- Code - Algorithme El Gamal/nombre_premier.py
+++ Code - Algorithme El Gamal/nombre_premier.py
@@ -1,11 +1,53 @@
+from random import random
+from time import perf_counter
+import matplotlib.pyplot as plt
+
+def est_premier(n):
+  if n == 2 or n == 3: return True
+  if n < 2 or n%2 == 0: return False
+  if n < 9: return True
+  if n%3 == 0: return False
+  r = int(n**0.5)
+  f = 5
+  while f <= r:
+    if n%f == 0: return False
+    if n%(f+2) == 0: return False
+    f +=6
+  return True 
+
+
+#Plus appliquable au dela de 20 chiffres
 def premier(n):
-    """ Renvoie le premier nombre premier inférieur à n."""
-    n += 1
-    tableau = [0,0] + [i for i in range(2, n)]
-    for i in range(2, n):
-        if tableau[i] != 0:
-            # c'est un nombre 1er: on garde, mais on neutralise ses multiples
-            for j in range(i*2, n, i):
-                tableau[j] = 0
-    L= [p for p in tableau if p!=0]
-    return L[len(L)-1]
+    while n>10:
+        if est_premier(n):
+            return n
+        n-=1
+    
+    
+def premier_Pollard(n):
+    eps=10**(-100)
+    while n>10:
+        if (n-1)/2-int((n-1)/2)<eps:
+            if est_premier(int((n-1)/2)):
+                if est_premier(n):
+                    return n
+        n-=1
+    
+def test(n):
+    X=[]
+    Y=[]
+    for i in range(1,n+1):
+        X.append(i)
+        k=0
+        for j in range(i):
+            k+=int(random()*10)*(10**j)
+        t1=perf_counter()
+        premier(k)
+        t2=perf_counter()
+        print(i,t2-t1)
+        Y.append(t2-t1)
+        
+    plt.plot(X,Y)
+    plt.show()
+            
+    
