36bfc797422702c569f5be32cfb96b7802cdddde
PetitBesson <35603264+PetitBesson@users.noreply.github.com>
2018-03-16 22:36:41 +0100

    le problème etait l'exponientation modulaire récursive

--- Code - Algorithme El Gamal/nombre_premier_probabiliste.py
+++ Code - Algorithme El Gamal/nombre_premier_probabiliste.py
@@ -1,13 +1,14 @@
-from random import randint
+import random as rd
+import time
 
 # Génération d'un nombre premier basée su le test de primalité de Miller-Rabin
 
 def MillerRabin_génération(n): #longueur en base 10 du nombre premier
     k=0 #candidat premier
     for i in range(n):
-        k+=randint(0,9)*10**i
+        k+=rd.randint(0,9)*10**i
 
-def puissmod(a,d,n):
+def puissmod(a,d,n): # récursif
 
     if d==0:
         return 1
@@ -20,17 +21,29 @@ def puissmod(a,d,n):
         b=(a**2)
         return a*puissmod(b,d//2,n)%n
     
-    
+def puissmod2(a,d,n): #itératif : beaucoup plus efficace (10^-5)
+    dbin=bin(d)
+    L=[int(dbin[-i-1]) for i in range(len(dbin)-2)]
+    res=1
+    while L!=[]:
+        k=L.pop(0)
+        if k>0:
+            res=res*a%n
+        a=a**2%n
+    return res
+
+
+
 def MillerRabin_temoin(a,n):
     #Calcul de s et d tels que n-1=2**s*d
-    d=(n-1)/2
+    d=(n-1)//2
     s=1
     while d%2==0:
-        d/=2
+        d=d//2
         s+=1
     
     #Premier test
-    x=puissmod(a,d,n)
+    x=puissmod2(a,d,n)
     if x==1 or x==n-1 :
         return False
     
@@ -48,9 +61,21 @@ def MillerRabin_temoin(a,n):
 def MillerRabin_test(n,k): #primalité de n a tester et k nombre de boucles
     
     for t in range(k):
-        a=randint(2,n-2)
+        a=rd.randint(2,n-2)
         if MillerRabin_temoin(a,n):
             return False
-        print(a,t)
     return True
-    
+
+def test(i):
+    n=rd.choice([1,3,5,7,9])
+    for k in range(1,i) :
+        n+=rd.randint(0,9)*10**k
+    a=rd.randint(0,n)
+    d=rd.randint(0,n)
+    print(a,d,n)
+    t1=time.clock()
+    puissmod(a,d,n)
+    t2=time.clock()
+    puissmod2(a,d,n)
+    t3=time.clock()
+    print(t2-t1,t3-t2)
