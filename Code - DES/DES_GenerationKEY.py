59060c0b661d6b5166b00ef5491f357d61cb64b1
Ludo <ludo@github.com>
2018-03-30 15:51:40 +0200

    up

--- Code - DES/DES_GenerationKEY.py
+++ Code - DES/DES_GenerationKEY.py
@@ -14,12 +14,35 @@ def permutationCP1(K):
 def rotationGauche(L):
     """ Rotation à gauche de L:  les bits en seconde position prennent la première position, ceux en troisième position la seconde, ...
 Les bits en première position passent en dernière position."""
-    L = []
+    n = len(L)
+    L.append(L[0])
+    L.pop(0)
+    return L
+
+def regroup(G,D):
+    return G+D
+
+def permutationCP2(K):
+    """Effectue la permutation CP2 du bloc de 56 bits en un bloc de 48 bits qui est la clé Ki"""
+    if len(K) != 56 : return "Erreur len(K)"
+    else:
+        Ki = []
+        CP2=[14,17, 11,	24,	 1,	5,	3,	28,	15,	6,	21,	10,
+            23,	19,	12,	 4,	26,	8,	16,	7,	27,	20,	13,	 2,
+            41,	52,	31,	37,	47,	55,	30,	40,	51,	45,	33,	48,
+            44,	49,	39,	56,	34,	53,	46,	42,	50,	36,	29,	32]
+        for i in CP2:
+            Ki.append(K[i-1])
+        return Ki
+
+def genKey(L):
 
 
 def gen(n):
-    return [(i+1)*10 for i in range(n)]
+    return ([(i+1)*10 for i in range(n)])
 
 K = gen(64)
 
+print(K)
 print(permutationCP1(K))
+print(rotationGauche(K))
