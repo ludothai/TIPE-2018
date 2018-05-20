e5a689c41284fb98cb84f7a5ec3fbbb9cc7b2665
PetitBesson <35603264+PetitBesson@users.noreply.github.com>
2018-05-20 15:43:17 +0200

    Correction erreur synthaxe

--- Code - Algorithme El Gamal/ElGamal.py
+++ Code - Algorithme El Gamal/ElGamal.py
@@ -1,6 +1,7 @@
 from random import randint
 
-def puissmod(a,d,n): '''a**d mod n'''
+def puissmod(a,d,n):
+    '''a**d mod n'''
     #itératif : beaucoup plus efficace (10^-5) 600 chiffres -> 0.024571632396359178 s
     dbin=bin(d)
     L=[int(dbin[-i-1]) for i in range(len(dbin)-2)]
@@ -12,7 +13,8 @@ def puissmod(a,d,n): '''a**d mod n'''
         a=a**2%n
     return res
 
-def generation_cle(p,g): '''p premier,g générateur'''
+def generation_cle(p,g):
+    '''p premier,g générateur'''
     cle_priv=randint(1,p-1)
     cle_publ=puissmod(g,cle_priv,p)
     return cle_priv,cle_publ
@@ -24,7 +26,7 @@ def chiffrement(p,g,cle_dest,message_clair,signature=True):
     entete=puissmod(g,k,p)
     return entete,message_crypt
 
-def dechiffrement(p,g,cle_priv,entete,message_crypt)
+def dechiffrement(p,g,cle_priv,entete,message_crypt):
     cle_sess=puissmod(entete,cle_priv,p)
     message_clair=DES(message_crypt,cle_sess)
     return message_clair
